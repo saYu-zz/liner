68efb77fcb23c28f4895e469064b118b029a8579
sa Yu <yujinqiuustb@gmail.com>
2011-12-16 23:09:44 +0800

    v1.1:comment out unneed

--- netstat.py
+++ netstat.py
@@ -2,8 +2,8 @@
  
 import pwd
 import os
-import re
-import glob
+#import re
+#import glob
  
 PROC_TCP = "/proc/net/tcp"
 STATE = {
@@ -29,6 +29,7 @@ def _load():
 	log.close()
 	return False
     content = f.readlines()
+    f.close()
     #remove header
     content.pop(0)
     return content
@@ -60,18 +61,20 @@ def netstat():
         line_array = _remove_empty(line.split(' '))     # Split lines and remove empty spaces.
         l_host,l_port = _convert_ip_port(line_array[1]) # Convert ipaddress and port from hex to decimal.
         r_host,r_port = _convert_ip_port(line_array[2]) 
-        tcp_id = line_array[0]
+        #tcp_id = line_array[0]
         state = STATE[line_array[3]]
-        uid = pwd.getpwuid(int(line_array[7]))[0]       # Get user from UID.
-        inode = line_array[9]                           # Need the inode to get process pid.
-        pid = _get_pid_of_inode(inode)                  # Get pid prom inode.
-        try:                                            # try read the process name.
-            exe = os.readlink('/proc/'+pid+'/exe')
-        except:
-            exe = None
+        #uid = pwd.getpwuid(int(line_array[7]))[0]       # Get user from UID. line_array[7]==>UID
+        #inode = line_array[9]                           # Need the inode to get process pid.
+        #pid = _get_pid_of_inode(inode)                  # Get pid prom inode.
+        #try:                                            # try read the process name.
+        #    exe = os.readlink('/proc/'+pid+'/exe')
+        #except:
+        #    exe = None
  
-        nline = [tcp_id, uid, l_host+':'+l_port, r_host+':'+r_port, state, pid, exe]
-        result.append(nline)
+        #nline = (tcp_id, uid, l_host+':'+l_port, r_host+':'+r_port, state, pid, exe)
+	if state != 'LISTEN':
+            nline = (l_host+':'+l_port, r_host+':'+r_port)
+            result.append(nline)
     return result
  
 def _get_pid_of_inode(inode):
@@ -79,7 +82,7 @@ def _get_pid_of_inode(inode):
     To retrieve the process pid, check every running process and look for one using
     the given inode.
     '''
-    for item in glob.glob('/proc/[0-9]*/fd/[0-9]*'):
+    for item in glob.glob('/proc/'+inode+'/fd/[0-9]*'):
         try:
             if re.search(inode,os.readlink(item)):
                 return item.split('/')[2]
